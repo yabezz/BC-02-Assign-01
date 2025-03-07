8e634d6013ceade2b7c8158a20304862d9297099
yabez <yabez.davidraj@psiog.com>
2025-03-07 10:20:52 +0530

    Files added

---
+++ javascript/script.js
@@ -0,0 +1,20 @@
+// JavaScript to trigger slide-in animation
+// const slideInElements = document.querySelectorAll(".slide-in");
+
+// function checkSlide() {
+//   slideInElements.forEach((element) => {
+//     const slideInAt =
+//       window.scrollY + window.innerHeight - element.clientHeight / 2;
+//     const elementBottom = element.offsetTop + element.clientHeight;
+//     const isHalfShown = slideInAt > element.offsetTop;
+//     const isNotScrolledPast = window.scrollY < elementBottom;
+
+//     if (isHalfShown && isNotScrolledPast) {
+//       element.classList.add("active");
+//     } else {
+//       element.classList.remove("active");
+//     }
+//   });
+// }
+
+// window.addEventListener("scroll", checkSlide);
